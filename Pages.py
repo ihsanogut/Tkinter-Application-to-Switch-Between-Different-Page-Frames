39e1bf700162446bba05bc46226f6fecfc38e57e
ihsan öğüt <ihsan.ogut@gmail.com>
2024-01-04 21:58:34 +0300

    Add files via upload

---
+++ Pages.py
@@ -0,0 +1,166 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+LARGEFONT = ("Verdana", 35)
+
+
+class tkinterApp(tk.Tk):
+
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # creating a container
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # initializing frames to an empty array
+        self.frames = {}
+
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (StartPage, Page1, Page2):
+            frame = F(container, self)
+
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with
+            # for loop
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+# first window frame startpage
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # label of frame Layout 2
+        label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+
+        # putting the grid in its place by using
+        # grid
+        label.grid(row=0, column=2, padx=10, pady=10)
+
+        button1 = ttk.Button(self, text="Page 1",
+                             command=lambda: controller.show_frame(Page1))
+
+        # putting the button in its place by
+        # using grid
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+        ## button to show frame 2 with text layout2
+        button2 = ttk.Button(self, text="Page 2",
+                             command=lambda: controller.show_frame(Page2))
+
+        # putting the button in its place by
+        # using grid
+        button2.grid(row=2, column=1, padx=10, pady=10)
+
+        listbox = Listbox(self,selectmode=MULTIPLE)
+        listbox.grid(row=1, column=2, padx=10, pady=10)
+
+        scrollbar = ttk.Scrollbar(self)
+        # Adding Scrollbar to the right
+
+        # Inserting the listbox items
+        listbox.insert(1, "Data Structure")
+        listbox.insert(2, "Algorithm")
+        listbox.insert(3, "Data Science")
+        listbox.insert(4, "Machine Learning")
+        listbox.insert(5, "Blockchain")
+
+
+        # Attaching Listbox to Scrollbar
+        # Since we need to have a vertical
+        # scroll we use yscrollcommand
+        listbox.config(yscrollcommand=scrollbar.set)
+
+        # setting scrollbar command parameter
+        # to listbox.yview method its yview because
+        # we need to have a vertical view
+        scrollbar.config(command=listbox.yview)
+
+        def selected_item():
+            # Traverse the tuple returned by
+            # curselection method and print
+            # corresponding value(s) in the listbox
+            for i in listbox.curselection():
+                print(listbox.get(i))
+
+            print(listbox.size())
+            listbox.selection_clear(0,listbox.size())
+
+        button3 = ttk.Button(self, text='Print Selected', command=selected_item)
+        button3.grid(row=2, column=2, padx=10, pady=10)
+
+# second window frame page1
+class Page1(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page 1", font=LARGEFONT)
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text="StartPage",
+                             command=lambda: controller.show_frame(StartPage))
+
+        # putting the button in its place
+        # by using grid
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+        # button to show frame 2 with text
+        # layout2
+        button2 = ttk.Button(self, text="Page 2",
+                             command=lambda: controller.show_frame(Page2))
+
+        # putting the button in its place by
+        # using grid
+        button2.grid(row=2, column=1, padx=10, pady=10)
+
+
+# third window frame page2
+class Page2(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text="Page 1",
+                             command=lambda: controller.show_frame(Page1))
+
+        # putting the button in its place by
+        # using grid
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+        # button to show frame 3 with text
+        # layout3
+        button2 = ttk.Button(self, text="Startpage",
+                             command=lambda: controller.show_frame(StartPage))
+
+        # putting the button in its place by
+        # using grid
+        button2.grid(row=2, column=1, padx=10, pady=10)
+
+
+# Driver Code
+app = tkinterApp()
+app.mainloop()
